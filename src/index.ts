5dea4d45e33e14974aa8401d82a43dc18e981c3e
David Sousa <david01sousa@Hotmail.com>
2023-07-21 10:46:26 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`running on port ${port}!`));
